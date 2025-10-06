fd321b3a3808d692836d935606e9e6db46e64b94
Norman <184494152+normanling1@users.noreply.github.com>
2025-10-06 17:02:24 +0100

    Updated code with access modifiers.

---
+++ Library/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class Book
+    {
+        string Title;
+        string Author;
+        string ISBN;
+
+        //Example of a constructor that allows us to 'construct' a new book object
+
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book Title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+
+        
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,52 +1,9 @@
-﻿class Book
+﻿using Library;
+class Program
 {
-    string Title;
-    string Author;
-    string ISBN;
-
-    //Example of a constructor that allows us to 'construct' a new book object
-
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-    }
-
-    void DisplayInfo()
-    {
-        Console.WriteLine($"Book Title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
 
     static void Main(string[] args)
     {
-
-
-        /*//Create a new instance (object) of the Book class
-        //Note how the name differs from the class namae
-
-        Book book = new Book();
-        
-        //This information is for one book in our library
-        book.Title = "C# For Beginners";
-        book.Author = "BillGates";
-        book.ISBN = "12345678";
-
-        //This is another book object.
-        //Note that the object name is 'book1' and not 'book'
-
-        Book book1 = new Book();
-        book1.Title = "C# Methods and Classes";
-        book1.Author = "Microsoft";
-        book1.ISBN = "55667788";
-
-        //Output book information to the console
-        book.DisplayInfo();
-        book1.DisplayInfo();*/
-
 
         Book book = new Book("C# For Beginners", "BillGates", "1234567");
 
