0f214af0751df8689514b3532f29b204a12a1a8d
Norman <184494152+normanling1@users.noreply.github.com>
2025-10-06 17:05:58 +0100

    Updated code with variables and properties.

--- Library/Book.cs
+++ Library/Book.cs
@@ -6,17 +6,33 @@ namespace Library
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title;
+        private string author;
+        private string isbn;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
 
         //Example of a constructor that allows us to 'construct' a new book object
 
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
         }
 
         public void DisplayInfo()
